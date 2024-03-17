91cbe53e8cf70281407437f81affb0b2c2c74937
Dima Moskalenko <dmtrmsklnk@gmail.com>
2024-03-18 01:26:34 +0200

    95% finish

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,7 +1,11 @@
-const burger = document.querySelector('.header__burger');
-const navButtonWrapper = document.querySelector('.header__nav-button-wrapper');
+const header = () => {
+    const burger = document.querySelector('.header__burger');
+    const navButtonWrapper = document.querySelector('.header__nav-button-wrapper');
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('header__burger_active');
-    navButtonWrapper.classList.toggle('header__nav-button-wrapper_active');
-   })
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('header__burger_active');
+        navButtonWrapper.classList.toggle('header__nav-button-wrapper_active');
+    })
+}
+
+export default header;
